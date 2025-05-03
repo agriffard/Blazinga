fc605b9f82ca175f7ad44d281d517a42b739e6e9
Antoine Griffard <agriffard@hotmail.com>
2025-05-03 12:11:57 +0200

    Version

--- src/Blazinga.Samples/VersionHelper.cs
+++ src/Blazinga.Samples/VersionHelper.cs
@@ -6,6 +6,12 @@ public class VersionHelper
     {
         var assembly = typeof(Blazinga.Components.EnumSelect<>).Assembly;
         var version = assembly.GetName().Version?.ToString() ?? "Unknown";
+        // Display only Major.Minor.Patch
+        var versionParts = version.Split('.');
+        if (versionParts.Length > 3)
+        {
+            version = string.Join(".", versionParts[0..3]);
+        }
         return version;
     }
 }
